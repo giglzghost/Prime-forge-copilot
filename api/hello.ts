771770a834909628c6fad4ec079031bc460c4da7
giglzghost <giglzghost@gmail.com>
2026-02-26 14:46:21 -0600

    Delete api/hello.ts

--- api/hello.ts
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    language: "TypeScript",
-    message: "TS serverless function OK",
-    time: new Date().toISOString()
-  });
-}
