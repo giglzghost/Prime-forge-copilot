7106b146ae777ea90e51ca19c90512388344e380
giglzghost <giglzghost@gmail.com>
2026-02-26 14:14:54 -0600

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "TS serverless OK" });
+  res.status(200).json({
+    language: "TypeScript",
+    message: "TS serverless function OK",
+    time: new Date().toISOString()
+  });
 }
