0604510a8c264090d2649f97f7d391f8ce73c6f0
giglzghost <giglzghost@gmail.com>
2026-03-01 23:02:12 -0600

    Create status.ts

---
+++ api/status.ts
@@ -0,0 +1,17 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { sendJson } from "./_utils";
+import { route } from "../src/core/router";
+
+export default async function handler(req: IncomingMessage | any, res: ServerResponse | any) {
+  if (req.method !== "GET") {
+    return sendJson(res, 405, { error: "Method not allowed" });
+  }
+
+  const result = route({
+    type: "status",
+    action: "get",
+    requestedBy: "api:status"
+  });
+
+  return sendJson(res, result.ok ? 200 : 500, result);
+}
