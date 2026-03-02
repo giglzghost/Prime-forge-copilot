4687577d500854dba5e393edcc13f8394ff90baa
giglzghost <giglzghost@gmail.com>
2026-03-02 16:55:50 -0600

    Update image.ts

--- api/image.ts
+++ api/image.ts
@@ -1,3 +1,8 @@
+export const config = {
+  runtime: "nodejs18.x"
+};
+
+
 import { IncomingMessage, ServerResponse } from "http";
 import { readJsonBody, sendJson } from "./_utils";
 import { generateImage } from "../src/ai/provider";
