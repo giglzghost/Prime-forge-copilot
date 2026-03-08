4215862d213ec1be04db8318a6d2ba89434bdf40
giglzghost <giglzghost@gmail.com>
2026-03-08 03:54:53 -0500

    Update hello-js.js

--- api/hello-js.js
+++ api/hello-js.js
@@ -2,8 +2,11 @@ export const config = {
   runtime: "nodejs"
 };
 
+import { IncomingMessage, ServerResponse } from "http";
+import { sendJson } from "./_utils";
+
 export default function handler(req, res) {
-  res.status(200).json({
+  return sendJson(res, 200, {
     language: "JavaScript",
     message: "JS serverless function OK",
     time: new Date().toISOString()
