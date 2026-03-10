41bbc657823a0994c0a58eed7a8dfa9895d6d034
giglzghost <giglzghost@gmail.com>
2026-03-09 19:54:24 -0500

    Delete api/hello-py.py

--- api/hello-py.py
+++
@@ -1,6 +0,0 @@
-def handler(request):
-    return {
-        "language": "Python",
-        "message": "Python serverless function OK",
-        "time": __import__("datetime").datetime.utcnow().isoformat()
-    }
