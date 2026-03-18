e955ee253d95cdb4bca4a3637f1c0d7c805d6b1d
giglzghost <giglzghost@gmail.com>
2026-03-18 00:38:29 -0500

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,8 @@
+// next-env.d.ts
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should be included in tsconfig "include".
+// Do not remove the triple-slash references above.
+
+export {};
